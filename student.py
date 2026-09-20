799f89ba3a54ae6527c329c6622a5c955db7b6d0
shreeshreeya69-crypto <shreeshreeya69@gmail.com>
2026-09-20 22:38:46 +0530

    Added age to student details

--- student.py
+++ student.py
@@ -2,6 +2,8 @@ print("Student Management System")
 
 name = input("Enter student name: ")
 roll_no = input("Enter roll number: ")
+age = input("Enter age: ")
 
 print("Student Name:", name)
 print("Roll Number:", roll_no)
+print("Age:", age)
