5178b94963fb913ee0336ee5b70cb3365215ec4d
shreeshreeya69-crypto <shreeshreeya69@gmail.com>
2026-09-23 20:00:02 +0530

    Day 2: Add student details input

--- student.py
+++ student.py
@@ -2,7 +2,16 @@ print("Student Management System")
 
 name = input("Enter student name: ")
 roll_no = input("Enter roll number: ")
-age = input("Enter age: ")
+while True:
+    try:
+        age = int(input("Enter student age: "))
+        if 1 <= age <= 100:
+            break
+        else:
+            print("Please enter a age between 1 and 100.")
+    except ValueError:
+        print("Please enter a number.")
+
 
 print("Student Name:", name)
 print("Roll Number:", roll_no)
